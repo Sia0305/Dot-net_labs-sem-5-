83cf0a8a93207d394b2eee9de2a259c8a25e22bb
siyakansagara <siyakansagara@gmail.com>
2026-07-09 16:32:35 +0530

    Demo

---
+++ Demo_Api/Data/AppDbcontext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Demo_Api.Models;
+
+namespace Demo_Api.Data
+{
+    public class AppDbcontext : DbContext
+    {
+        public AppDbcontext(DbContextOptions<AppDbcontext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Student> Students => Set<Student>();
+
+       
+    }
+}
---
+++ Demo_Api/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo_Api.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+    }
+}
